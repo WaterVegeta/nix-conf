9d224d5966a90699d6c359d3587ae4e209079bae
nice <gopou25@gmail.com>
2026-08-14 16:27:09 +0300

    nixvim config

--- modules/features/nvim/_nixvim-config.nix
+++ modules/features/nvim/_nixvim-config.nix
@@ -21,11 +21,29 @@
 
     ];
  
+    colorschemes = {
+	tokyonight={
+	    enable = true;
+	    #settings.transparent = true;
+	    #settings.styles = {
+	    #    sidebars = "transparent";
+	    #    floats = "transparent";
+	    #};
+	};
+    };
       # Enable plugins declaratively
     plugins = {
 	lualine.enable = true;
         treesitter.enable = true;
-        
+        telescope = {
+	    enable = true;
+	    keymaps = {
+		"<leader>ff" = "find_files";
+		"<leader>fg" = "live_grep";
+		"<leader>fb" = "buffers";
+		"<leader>fh" = "help_tags";
+	    };
+	};
         # Example LSP setup
         lsp = {
           enable = true;
